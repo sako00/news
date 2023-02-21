a84fd955434066f23925d49c4ea7e83c5214f66f
sako00 <sanna.erv@hotmail.com>
2023-02-21 17:35:23 +0200

    nwe app and code

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
-import logo from './logo.svg';
 import './App.css';
+import {useEffect, useState} from 'react'
+import axios from 'axios'
+import Detail from './Detail';
+
+const URL = 'https://newsapi.org/v2'
+const  APIKEY = 'd4fd82b33f5a45988cbb0680fdc2b819' 
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [error,setError] = useState(null);
+  const [isLoaded,setIsLoaded] = useState(false);
+  const [items,setItems] = useState([]);
+  const [selecteditem,setSelectedItem] = useState(null);
+
+  useEffect (() =>{
+    const criteria = 'top-headlines?country=us&category=business';
+    const address = URL + '/'+ criteria + '&apikey=' + APIKEY;
+    
+    axios.get(address)
+    .then((response) =>{
+      setError(null);
+      setIsLoaded(true);
+      setItems(response.data.articles);
+    }).catch(error =>{
+      alert(error);
+    })
+  }, [])
+
+  function close(){
+    setSelectedItem(null)
+  }
+  if(selecteditem !=null){
+    return <Detail
+    title={selecteditem.title}
+    image={selecteditem.urlToImage}
+    description={selecteditem.description}
+    close={close}
+    />
+  }
+  if(error){
+    return <p>{error.message}</p>
+  }
+  else if(!isLoaded){
+    return<p>Loading..</p>
+  }
+  else{
+    return(
+      <div>
+        {items?.map(item =>(
+          <div key={item.title} onClick={e=> setSelectedItem(item)}>
+            <h3>{item.title}</h3>
+            <img src={item.urlToImage}></img>
+            <p>{item.description}</p>
+            </div>
+        ))}
+
+      </div>
+    )
+  }
 }
+  
 
 export default App;
